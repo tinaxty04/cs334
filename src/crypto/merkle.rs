9512ab8bd7bf9e20986c1effb20e4c5f3766a941
asdfghjk20222 <tinaxu2026@u.northwestern.edu>
2025-04-20 18:42:44 -0500

    Finished Part 1: Merkle tree and transaction sign/verify

--- src/crypto/merkle.rs
+++ src/crypto/merkle.rs
@@ -1,29 +1,129 @@
 use super::hash::{Hashable, H256};
+use ring::digest;
+
+/// A Merkle tree node
+#[derive(Debug, Default, Clone)]
+struct MerkleTreeNode {
+    left: Option<Box<MerkleTreeNode>>,
+    right: Option<Box<MerkleTreeNode>>,
+    hash: H256,
+}
 
 /// A Merkle tree.
 #[derive(Debug, Default)]
 pub struct MerkleTree {
+    root: MerkleTreeNode,
+    level_count: usize, // how many levels the tree has
+}
+
+/// Given the hash of the left and right nodes, compute the hash of the parent node.
+fn hash_children(left: &H256, right: &H256) -> H256 {
+    let mut concatenated = Vec::new();
+    concatenated.extend_from_slice(left.as_ref());
+    concatenated.extend_from_slice(right.as_ref());
+    digest::digest(&digest::SHA256, &concatenated).into()
+}
+
+/// Duplicate the last node in `nodes` to make its length even.
+fn duplicate_last_node(nodes: &mut Vec<Option<MerkleTreeNode>>) {
+    if let Some(last_node) = nodes.last().cloned() {
+        nodes.push(last_node);
+    }
 }
 
 impl MerkleTree {
     pub fn new<T>(data: &[T]) -> Self where T: Hashable, {
-        unimplemented!()
+        assert!(!data.is_empty());
+
+        // create the leaf nodes:
+        let mut curr_level: Vec<Option<MerkleTreeNode>> = Vec::new();
+        for item in data {
+            curr_level.push(Some(MerkleTreeNode { hash: item.hash(), left: None, right: None }));
+        }
+        let mut level_count = 1;
+        
+        // create the upper levels of the tree:
+        while curr_level.len() > 1 {
+            // Whenever a level of the tree has odd number of nodes, duplicate the last node to make the number even:
+            if curr_level.len() % 2 == 1 {
+                duplicate_last_node(&mut curr_level);
+            }
+            assert_eq!(curr_level.len() % 2, 0); // make sure we now have even number of nodes.
+
+            let mut next_level: Vec<Option<MerkleTreeNode>> = Vec::new();
+            for i in 0..curr_level.len() / 2 {
+                let left = curr_level[i * 2].take().unwrap();
+                let right = curr_level[i * 2 + 1].take().unwrap();
+                let hash = hash_children(&left.hash, &right.hash);
+                next_level.push(Some(MerkleTreeNode { hash: hash, left: Some(Box::new(left)), right: Some(Box::new(right)) }));
+            }
+            curr_level = next_level;
+            level_count += 1;
+        }
+        MerkleTree {
+            root: curr_level[0].take().unwrap(),
+            level_count: level_count,
+        }
     }
 
     pub fn root(&self) -> H256 {
-        unimplemented!()
+        self.root.hash
     }
 
     /// Returns the Merkle Proof of data at index i
     pub fn proof(&self, index: usize) -> Vec<H256> {
-        unimplemented!()
+        let mut proof = Vec::new();
+        let mut current_index = index;
+        
+        // Helper function to collect proof recursively
+        fn collect_proof(node: &MerkleTreeNode, target_index: &mut usize, proof: &mut Vec<H256>) {
+            if let (Some(left), Some(right)) = (&node.left, &node.right) {
+                let is_left = *target_index % 2 == 0;
+                if is_left {
+                    // Target is in left subtree, add right hash to proof
+                    proof.push(right.hash);
+                } else {
+                    // Target is in right subtree, add left hash to proof
+                    proof.push(left.hash);
+                }
+                *target_index /= 2;
+                
+                // Continue with the appropriate subtree
+                if is_left {
+                    collect_proof(left, target_index, proof);
+                } else {
+                    collect_proof(right, target_index, proof);
+                }
+            }
+        }
+        
+        collect_proof(&self.root, &mut current_index, &mut proof);
+        proof
     }
 }
 
 /// Verify that the datum hash with a vector of proofs will produce the Merkle root. Also need the
 /// index of datum and `leaf_size`, the total number of leaves.
 pub fn verify(root: &H256, datum: &H256, proof: &[H256], index: usize, leaf_size: usize) -> bool {
-    unimplemented!()
+    let mut binary_index = Vec::new();
+    let mut index = index;
+    for _ in 0..proof.len() {
+        binary_index.push(index % 2);
+        index /= 2;
+    }
+    let mut curr_hash = *datum;
+    for i in 0..proof.len() {
+        curr_hash = match binary_index[i] {
+            0 => { // data is on the left, sibling on the right:
+                hash_children(&curr_hash, &proof[i])
+            },
+            1 => { // data is on the right, sibling on the left:
+                hash_children(&proof[i], &curr_hash)
+            },
+            _ => unreachable!(),
+        };
+    }
+    *root == curr_hash
 }
 
 #[cfg(test)]
--- src/transaction.rs
+++ src/transaction.rs
@@ -1,28 +1,65 @@
 use serde::{Serialize,Deserialize};
-use ring::signature::{Ed25519KeyPair, Signature, KeyPair, VerificationAlgorithm, EdDSAParameters};
+use ring::signature::{Ed25519KeyPair, Signature, KeyPair, VerificationAlgorithm, EdDSAParameters, ED25519};
+use bincode;
+use rand::random;
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Input {
+    pub previous_output: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Output {
+    pub value: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct RawTransaction {
+    pub inputs: Vec<Input>,
+    pub outputs: Vec<Output>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct SignedTransaction {
+    pub transaction: RawTransaction,
+    pub signature: Vec<u8>, // bytes of signature
+    pub public_key: Vec<u8>, // public key needed to verify
 }
 
 /// Create digital signature of a transaction
 pub fn sign(t: &RawTransaction, key: &Ed25519KeyPair) -> Signature {
-    unimplemented!()
+    let serialized = bincode::serialize(t).unwrap();
+    key.sign(&serialized)
 }
+
 
 /// Verify digital signature of a transaction, using public key instead of secret key
 pub fn verify(t: &RawTransaction, public_key: &<Ed25519KeyPair as KeyPair>::PublicKey, signature: &Signature) -> bool {
-    unimplemented!()
+    let serialized = bincode::serialize(t).unwrap();
+    let unparsed = ring::signature::UnparsedPublicKey::new(&ring::signature::ED25519, public_key.as_ref());
+    unparsed.verify(&serialized, signature.as_ref()).is_ok()
 }
 
 #[cfg(any(test, test_utilities))]
 mod tests {
     use super::*;
     use crate::crypto::key_pair;
+    use rand::random;
 
     pub fn generate_random_transaction() -> RawTransaction {
-        Default::default()
-        //unimplemented!()
+        // Create a simple transaction with just one input and one output
+        let input = Input {
+            previous_output: random(), // Random u32
+        };
+
+        let output = Output {
+            value: random(), // Random u32
+        };
+
+        RawTransaction {
+            inputs: vec![input],
+            outputs: vec![output],
+        }
     }
 
     #[test]
@@ -33,3 +70,4 @@ mod tests {
         assert!(verify(&t, &(key.public_key()), &signature));
     }
 }
+
